34a0943567fb6719825bc73ad97e77067c1a338b
José Rafael <rafael@labsit.io>
2021-10-11 23:25:04 -0300

    Criação de estrutura de teste unitários no módulo de persistence

--- currency-converter/currency-converter-persistence/src/main/java/br/com/jrafael/currencyconverter/persistence/repository/CurrencyTransactionEntityRepository.java
+++ currency-converter/currency-converter-persistence/src/main/java/br/com/jrafael/currencyconverter/persistence/repository/CurrencyTransactionEntityRepository.java
@@ -3,5 +3,5 @@ package br.com.jrafael.currencyconverter.persistence.repository;
 import br.com.jrafael.currencyconverter.persistence.model.CurrencyTransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CurrencyTransactionEntityRepository extends JpaRepository<CurrencyTransactionEntity, Long> {
+public interface CurrencyTransactionEntityRepository extends JpaRepository<CurrencyTransactionEntity, String> {
 }
--- currency-converter/currency-converter-persistence/src/test/java/br/com/jrafael/currencyconverter/persistence/CurrencyTransactionRepositoryTest.java
+++ currency-converter/currency-converter-persistence/src/test/java/br/com/jrafael/currencyconverter/persistence/CurrencyTransactionRepositoryTest.java
@@ -1,4 +1,60 @@
 package br.com.jrafael.currencyconverter.persistence;
 
+import br.com.jrafael.currencyconverter.domain.constants.FinanceCoins;
+import br.com.jrafael.currencyconverter.persistence.configuration.RepositoryTestConfiguration;
+import br.com.jrafael.currencyconverter.persistence.model.CurrencyTransactionEntity;
+import br.com.jrafael.currencyconverter.persistence.repository.CurrencyTransactionEntityRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Import(RepositoryTestConfiguration.class)
+@ContextConfiguration(classes = {CurrencyTransactionEntityRepository.class})
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class CurrencyTransactionRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private CurrencyTransactionEntityRepository repository;
+
+    private CurrencyTransactionEntity currencyTransactionEntity;
+
+    @BeforeEach
+    public void setUp() {
+        this.currencyTransactionEntity = CurrencyTransactionEntity.builder()
+                .currencyOrigin(FinanceCoins.EUR)
+                .destinationCurrency(FinanceCoins.BRL)
+                .date(LocalDateTime.now())
+                .conversionRate(BigDecimal.ONE)
+                .userId("2849384938493894832dmsmdksjds")
+                .sourceValue(new BigDecimal("9"))
+                .id("djfkejfkejfkejfkejkfjek")
+                .build();
+    }
+
+    @Test
+    public void saveTest() {
+        this.currencyTransactionEntity = this.repository.save(this.currencyTransactionEntity);
+        Optional<CurrencyTransactionEntity> result = this.repository.findById(this.currencyTransactionEntity.getId());
+        assertTrue(result.isPresent());
+        assertTrue(result.get().getCurrencyOrigin().equals(result.get().getCurrencyOrigin()));
+        assertTrue(result.get().getDestinationCurrency().equals(result.get().getDestinationCurrency()));
+        assertTrue(result.get().getSourceValue().equals(result.get().getSourceValue()));
+    }
+
 }
